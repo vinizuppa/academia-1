2c98ea230ca2fcf1cc469e2920516f75409e6d8a
Vinicius Zuppa <vcordeiro12@gmail.com>
2021-08-25 18:23:27 -0300

    Restrição: Aluno só recupera sua avaliações

--- src/main/java/com/daniloperez/academia/repositories/AvaliacaoAlunoRepository.java
+++ src/main/java/com/daniloperez/academia/repositories/AvaliacaoAlunoRepository.java
@@ -1,12 +1,18 @@
 package com.daniloperez.academia.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.daniloperez.academia.domain.Aluno;
 import com.daniloperez.academia.domain.AvaliacaoAluno;
 
 
 //Aqui iremos realizar acesso a dados. Pode ser: Buscar, salvar, deletar,
 @Repository
 public interface AvaliacaoAlunoRepository extends JpaRepository<AvaliacaoAluno, Integer>{
+	@Transactional(readOnly=true)
+	Page<AvaliacaoAluno> findByAluno(Aluno aluno, Pageable pageRequest);
 }
--- src/main/java/com/daniloperez/academia/services/AvaliacaoAlunoService.java
+++ src/main/java/com/daniloperez/academia/services/AvaliacaoAlunoService.java
@@ -6,16 +6,25 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.daniloperez.academia.domain.Aluno;
 import com.daniloperez.academia.domain.AvaliacaoAluno;
 import com.daniloperez.academia.repositories.AvaliacaoAlunoRepository;
+import com.daniloperez.academia.security.UserSS;
+import com.daniloperez.academia.services.exceptions.AuthorizationException;
 import com.daniloperez.academia.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class AvaliacaoAlunoService {
 	@Autowired
 	AvaliacaoAlunoRepository repo;
+	
+	@Autowired
+	private AlunoService alunoService;
 	
 	//Buscar AvaliacaoAluno por ID
 	public AvaliacaoAluno find(Integer id) {
@@ -35,5 +44,18 @@ public class AvaliacaoAlunoService {
 	public void delete(Integer id) {
 		find(id);
 		repo.deleteById(id);
+	}
+	
+	//Buscar AvaliacoesAluno somente do aluno que está logado 
+	public Page<AvaliacaoAluno> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		//Verifica se o usuário está autenticado.
+		if(user==null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+				
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Aluno aluno = alunoService.find(user.getId());
+		return repo.findByAluno(aluno, pageRequest);
 	}
 }
