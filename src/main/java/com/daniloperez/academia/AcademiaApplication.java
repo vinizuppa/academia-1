de3117108ec782f3df142f16f1ed23e0e8dc5b69
Vinicius Zuppa <vcordeiro12@gmail.com>
2021-08-26 16:32:10 -0300

    Configurando para salvar arquivos no S3

--- src/main/java/com/daniloperez/academia/AcademiaApplication.java
+++ src/main/java/com/daniloperez/academia/AcademiaApplication.java
@@ -1,12 +1,17 @@
 package com.daniloperez.academia;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.daniloperez.academia.services.S3Service;
+
 @SpringBootApplication
 public class AcademiaApplication implements CommandLineRunner{
 	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AcademiaApplication.class, args);
@@ -14,6 +19,6 @@ public class AcademiaApplication implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\temp\\fotos\\teste.jpg");
 	}
 }
